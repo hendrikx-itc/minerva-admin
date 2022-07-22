84f33681d67f84ac3f735ec060295fa5754518c7
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2022-07-22 09:59:20 +0200

    Initial working web service endpoint

--- crates/admin-service/main.rs
+++ crates/admin-service/main.rs
@@ -1,37 +1,48 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, middleware::Logger,};
 
 use bb8;
 use bb8_postgres::{tokio_postgres::NoTls, PostgresConnectionManager};
 
-#[get("/trendviewmaterializations/{name}")]
-async fn greet(pool: web::Data<bb8::Pool<PostgresConnectionManager<NoTls>>>, name: web::Path<String>) -> impl Responder {
-    let conn = pool.get().await.expect("could not get connection from pool");
+use utoipa::{
+    OpenApi,
+};
+use utoipa_swagger_ui::SwaggerUi;
 
-    let rows = conn.query(
-        "SELECT tsp.name FROM trend_directory.materialization m JOIN trend_directory.view_materialization vm ON vm.materialization_id = m.id JOIN trend_directory.trend_store_part tsp ON tsp.id = m.dst_trend_store_part_id",
-        &[&name.to_string()]
-    ).await.unwrap();
 
-    let 
-    for row in rows {
-    
-        let value: String = row.get(0);
-        format!("Hello {}!", value)
-    }
+mod trendviewmaterialization;
 
-}
+use trendviewmaterialization::{TrendMaterializationSource, TrendViewMaterialization, get_trend_view_materializations};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init();
+
+    #[derive(OpenApi)]
+    #[openapi(
+        handlers(
+            trendviewmaterialization::get_trend_view_materializations,
+        ),
+        components(TrendMaterializationSource, TrendViewMaterialization),
+        tags(
+            (name = "Trend Materialization", description = "Trend materialization management endpoints.")
+        ),
+    )]
+    struct ApiDoc;
+
     let manager = PostgresConnectionManager::new(
         "host=127.0.0.1 user=postgres".parse().unwrap(),
         NoTls,
     );
     let pool = bb8::Pool::builder().build(manager).await.unwrap();
 
+    let openapi = ApiDoc::openapi();
+
     HttpServer::new(move || {
-        App::new().app_data(web::Data::new(pool.clone()))
-            .service(greet)
+        App::new()
+            .wrap(Logger::default())
+            .app_data(web::Data::new(pool.clone()))
+            .service(SwaggerUi::new("/swagger-ui/{_:.*}").url("/api-doc/openapi.json", openapi.clone()))
+            .service(get_trend_view_materializations)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
