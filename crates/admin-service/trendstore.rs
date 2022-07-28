c62c9769b62817a30b1cf638719cfeaca65dc858
Andre Engels <andre.engels@hendrikx-itc.nl>
2022-07-28 11:01:35 +0200

    add showing single trend store

--- crates/admin-service/trendstore.rs
+++ crates/admin-service/trendstore.rs
@@ -6,7 +6,6 @@ use bb8_postgres::{tokio_postgres::NoTls, PostgresConnectionManager};
 use actix_web::{get, web::Data, web::Path, HttpResponse, Responder};
 
 use serde::{Deserialize, Serialize};
-
 use utoipa::Component;
 
 use minerva::interval::parse_interval;
@@ -137,7 +136,8 @@ pub(super) async fn get_trend_store_parts(
 
 #[utoipa::path(
     responses(
-	(status = 200, description = "Get a specific trend store part", body = TrendStorePart)
+	(status = 200, description = "Get a specific trend store part", body = TrendStorePart),
+	(status = 404, description = "Trend store part not found", body = String)
     )
 )]
 #[get("/trend-store-parts/{id}")]
@@ -303,4 +303,111 @@ pub(super) async fn get_trend_stores(
     };
 
     HttpResponse::Ok().json(m)
+}
+
+#[utoipa::path(
+    responses(
+	(status = 200, description = "Get a specific trend store", body = TrendStorePart),
+	(status = 404, description = "Trend store not found", body = String)
+    )
+)]
+#[get("/trend-stores/{id}")]
+pub(super) async fn get_trend_store(
+    pool: Data<Pool<PostgresConnectionManager<NoTls>>>,
+    id: Path<i32>,
+) -> impl Responder {
+    let tsid = id.into_inner();
+
+    let client = pool.get().await.unwrap();
+
+    let query_result = client
+        .query_one(
+            "SELECT ts.id, entity_type.name, data_source.name, granularity::text, partition_size::text, retention_period::text FROM trend_directory.trend_store ts JOIN directory.entity_type ON ts.entity_type_id = entity_type.id JOIN directory.data_source ON ts.data_source_id = data_source.id WHERE ts.id = $1",
+            &[&tsid],
+        )
+        .await;
+
+    match query_result {
+        Ok(row) => {
+            let mut trends: Vec<Trend> = vec![];
+            for inner_row in client.query("SELECT t.id, t.trend_store_part_id, t.name, t.data_type, t.time_aggregation, t.entity_aggregation, t.extra_data, t.description FROM trend_directory.table_trend t JOIN trend_directory.trend_store_part tsp ON t.trend_store_part_id = tsp.id WHERE tsp.trend_store_id = $1", &[&tsid]).await.unwrap() {
+		let trend = Trend {
+		    id: inner_row.get(0),
+		    trend_store_part: inner_row.get(1),
+		    name: inner_row.get(2),
+		    data_type: inner_row.get(3),
+		    time_aggregation: inner_row.get(4),
+		    entity_aggregation: inner_row.get(5),
+		    extra_data: inner_row.get(6),
+		    description: inner_row.get(7)
+		};
+		trends.push(trend)
+	    };
+
+            let mut generated_trends: Vec<GeneratedTrend> = vec![];
+            for inner_row in client.query("SELECT t.id, t.trend_store_part_id, t.name, t.data_type, t.expression, t.extra_data, t.description FROM trend_directory.generated_table_trend t JOIN trend_directory.trend_store_part tsp ON t.trend_store_part_id = tsp.id WHERE tsp.trend_store_id = $1", &[&tsid]).await.unwrap() {
+		let trend = GeneratedTrend {
+		    id: inner_row.get(0),
+		    trend_store_part: inner_row.get(1),
+		    name: inner_row.get(2),
+		    data_type: inner_row.get(3),
+		    expression: inner_row.get(4),
+		    extra_data: inner_row.get(5),
+		    description: inner_row.get(6)
+		};
+		generated_trends.push(trend)
+	    };
+
+            let mut parts: Vec<TrendStorePart> = vec![];
+
+            for inner_row in client
+		.query(
+		    "SELECT id, name, trend_store_id FROM trend_directory.trend_store_part WHERE trend_store_id = $1",
+		    &[&tsid],
+		)
+		.await
+		.unwrap()
+	    {
+		let tspid: i32 = inner_row.get(0);
+
+		let mut my_trends: Vec<Trend> = vec![];
+		for trend in &trends {
+		    if trend.trend_store_part == tspid {
+			my_trends.push(trend.clone())
+		    }
+		}
+
+		let mut my_generated_trends: Vec<GeneratedTrend> = vec![];
+		for generated_trend in &generated_trends {
+		    if generated_trend.trend_store_part == tspid {
+			my_generated_trends.push(generated_trend.clone())
+		    }
+		}
+
+		let trendstorepart = TrendStorePart {
+		    id: tspid,
+		    name: inner_row.get(1),
+		    trend_store: inner_row.get(2),
+		    trends: my_trends,
+		    generated_trends: my_generated_trends,
+		};
+		parts.push(trendstorepart)
+	    }
+
+            let trendstore = TrendStore {
+                id: tsid,
+                entity_type: row.get(1),
+                data_source: row.get(2),
+                granularity: parse_interval(row.get(3)).unwrap(),
+                partition_size: parse_interval(row.get(4)).unwrap(),
+                retention_period: parse_interval(row.get(5)).unwrap(),
+                trend_store_parts: parts,
+            };
+
+            HttpResponse::Ok().json(trendstore)
+        }
+        Err(_e) => {
+            HttpResponse::NotFound().body(format!("Trend store with id {} not found", &tsid))
+        }
+    }
 }
