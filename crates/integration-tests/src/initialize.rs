49ae66e1418b6f0ea1c5cd2166f6b16fbffdf34b
Alfred Blokland <alfred.blokland@1optic.io>
2023-04-20 16:54:35 +0200

    Wrap tests in test modules

--- crates/integration-tests/src/initialize.rs
+++ crates/integration-tests/src/initialize.rs
@@ -1,36 +1,37 @@
-use std::process::Command;
-
-use assert_cmd::prelude::*;
-use predicates::prelude::*;
-
-use minerva::database::{connect_db, create_database, drop_database};
-
 #[cfg(test)]
-#[tokio::test]
-async fn initialize() -> Result<(), Box<dyn std::error::Error>> {
-    let database_name = "minerva";
-    let mut client = connect_db().await?;
+mod tests {
+    use std::process::Command;
 
-    drop_database(&mut client, database_name).await?;
-    create_database(&mut client, database_name).await?;
+    use assert_cmd::prelude::*;
+    use predicates::prelude::*;
 
-    println!("Dropped database");
-    let mut cmd = Command::cargo_bin("minerva-admin")?;
-    cmd.env("PGDATABASE", database_name);
+    use minerva::database::{connect_db, create_database, drop_database};
 
-    let instance_root_path = std::fs::canonicalize("../../examples/tiny_instance_v1").unwrap();
+    #[tokio::test]
+    async fn initialize() -> Result<(), Box<dyn std::error::Error>> {
+        let database_name = "minerva";
+        let mut client = connect_db().await?;
 
-    cmd.arg("initialize").arg("--create-schema").arg("--with-definition").arg(&instance_root_path);
-    cmd.assert()
-        .success()
-        .stdout(predicate::str::contains("Created trigger"));
+        drop_database(&mut client, database_name).await?;
+        create_database(&mut client, database_name).await?;
 
-    let mut client = connect_db().await?;
+        println!("Dropped database");
+        let mut cmd = Command::cargo_bin("minerva-admin")?;
+        cmd.env("PGDATABASE", database_name);
 
-    drop_database(&mut client, database_name).await?;
+        let instance_root_path = std::fs::canonicalize("../../examples/tiny_instance_v1").unwrap();
 
-    println!("Dropped database");
+        cmd.arg("initialize").arg("--create-schema").arg("--with-definition").arg(&instance_root_path);
+        cmd.assert()
+            .success()
+            .stdout(predicate::str::contains("Created trigger"));
 
-    Ok(())
+        let mut client = connect_db().await?;
+
+        drop_database(&mut client, database_name).await?;
+
+        println!("Dropped database");
+
+        Ok(())
+    }
 }
-
