0b6ea39a55137660fe3c899d88d9f05df9339e1f
Alfred Blokland <alfred.blokland@hendrikx-itc.nl>
2024-01-04 10:40:20 +0100

    Handle interval parse errors

--- crates/minerva/src/interval.rs
+++ crates/minerva/src/interval.rs
@@ -1,8 +1,11 @@
 use std::time::Duration;
 
+use super::error::{Error, RuntimeError};
+
 use regex::Regex;
 
-pub fn parse_interval(interval_str: &str) -> Result<Duration, humantime::DurationError> {
+pub fn parse_interval(interval_str: &str) -> Result<Duration, Error> {
+    let month_re = Regex::new("mon(s|ths)?").unwrap();
     let interval_re = Regex::new(r"^(\d{2}):(\d{2}):(\d{2})$").unwrap();
 
     let capture_result = interval_re.captures(interval_str);
@@ -11,13 +14,11 @@ pub fn parse_interval(interval_str: &str) -> Result<Duration, humantime::Duratio
         Some(cap) => {
             format!("{} hours {} minutes {} seconds", &cap[1], &cap[2], &cap[3])
         }
-        None => interval_str
-            .replace("mon", "month")
-            .replace("mons", "month")
-            .replace("monthth", "month"),
+        None => month_re.replace(interval_str, "month").to_string()
     };
 
     humantime::parse_duration(&interval_str)
+        .map_err(|e| Error::Runtime(RuntimeError::from_msg(format!("Could not parse {interval_str} as interval: {e}"))))
 }
 
 #[cfg(test)]
@@ -29,6 +30,12 @@ mod tests {
         let dur = parse_interval("00:01:00").unwrap();
 
         let expected_dur = Duration::new(60, 0);
+
+        assert_eq!(dur, expected_dur);
+
+        let dur = parse_interval("2 months 29 days").unwrap();
+
+        let expected_dur = Duration::new(7765632, 0);
 
         assert_eq!(dur, expected_dur);
     }
